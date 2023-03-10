902a14711975e79a3362f2632ddaeb53ecb9415c
vesper <lesin.bogdan1234@gmail.com>
2023-03-10 18:20:27 +0300

    Added integration tests for endpoints: /tests/saveTest; /tests/load/{name}

--- src/test/java/ru/kubsu/geocoder/controller/TestControllerTest.java
+++ src/test/java/ru/kubsu/geocoder/controller/TestControllerTest.java
@@ -3,6 +3,7 @@ package ru.kubsu.geocoder.controller;
 import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.kubsu.geocoder.dto.RestApiError;
+import ru.kubsu.geocoder.model.Mark;
+import ru.kubsu.geocoder.repository.TestRepository;
 
 import java.util.HashMap;
 
@@ -21,6 +24,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestControllerTest {
     @LocalServerPort
     private Integer port;
+
+    @Autowired
+    private TestRepository testRepository;
     private final TestRestTemplate testRestTemplate = new TestRestTemplate();
 
     @BeforeAll
@@ -102,6 +108,82 @@ class TestControllerTest {
        assertEquals("/tests/check/str", body.getPath());
        assertEquals("Bad Request", body.getError());
        assertEquals(400, body.getStatus());
+    }
 
+    @Test
+    void integrationTestSaveTestModel() { // Positive Test
+        ResponseEntity<Void> response = testRestTemplate
+                .exchange("http://localhost:" + this.port + "/tests/saveTest?name=test",
+                HttpMethod.GET, null, Void.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void integrationTestSaveTestModelWhenNameIsNull() { // Negative Test
+       ResponseEntity<Void> response = testRestTemplate
+               .exchange("http://localhost:" + this.port + "/tests/saveTest",
+               HttpMethod.GET, null, Void.class);
+
+       assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void integrationTestSaveTestModelWhenExists() { // Negative Test
+        ru.kubsu.geocoder.model.Test test =  new ru.kubsu.geocoder.model.Test();
+        String nameTest = "newtest";
+        test.setName(nameTest);
+        testRepository.save(test);
+
+        ResponseEntity<Void> response = testRestTemplate
+                .exchange("http://localhost:" + this.port + "/tests/saveTest?name="+nameTest,
+                        HttpMethod.GET, null, Void.class);
+        //Bad error, need try-catch :/
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+    @Test
+    void integrationTestLoadTestModel() { // Positive Test
+       ru.kubsu.geocoder.model.Test test =  new ru.kubsu.geocoder.model.Test();
+       String nameTest = "newtest111";
+       test.setName(nameTest);
+       test.setDone(true);
+       test.setMark(Mark.B);
+       testRepository.save(test);
+
+       ResponseEntity<ru.kubsu.geocoder.model.Test> response = testRestTemplate
+               .exchange("http://localhost:"+this.port+"/tests/load/"+nameTest,
+                       HttpMethod.GET, null, ru.kubsu.geocoder.model.Test.class);
+
+       assertEquals(HttpStatus.OK, response.getStatusCode());
+
+       final ru.kubsu.geocoder.model.Test body = response.getBody();
+       if(body == null) {
+           fail("Body is null");
+       }
+
+       assertEquals(nameTest, body.getName());
+       assertTrue(body.getDone());
+       assertEquals(Mark.B, body.getMark());
+    }
+
+    @Test
+    void integrationTestLoadTestModelWhenNoRecord() {
+        ResponseEntity<ru.kubsu.geocoder.model.Test> response = testRestTemplate
+                .exchange("http://localhost:"+this.port+"/tests/load/teestt",
+                        HttpMethod.GET, null, ru.kubsu.geocoder.model.Test.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        if(response.getBody() != null) {
+            fail("No record. Body must be null");
+        }
+    }
+
+    @Test
+    void integrationTestLoadTestModelBadURL() {
+        ResponseEntity<ru.kubsu.geocoder.model.Test> response = testRestTemplate
+                .exchange("http://localhost:"+this.port+"/tests/load",
+                        HttpMethod.GET, null, ru.kubsu.geocoder.model.Test.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
